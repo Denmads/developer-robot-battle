2f231cf139c60cd396bed51fd42809fa8ba21415
Denmads <43139465+Denmads@users.noreply.github.com>
2025-10-01 21:54:55 +0200

    changes in my robot

--- client/my_robot.py
+++ client/my_robot.py
@@ -8,10 +8,10 @@ class MyRobot(RobotInterface):
     def build_robot(self, builder: RobotBuilder) -> None:
         builder.hull = RobotHullType.SPEEDY
         
-        builder.weapons.append(Weapon("wf", 0.5, -0.5, 315))
-        builder.weapons.append(Weapon("wr", -0.5, -0.5, 225))
-        builder.weapons.append(Weapon("wb", -0.5, 0.5, 135))
-        builder.weapons.append(Weapon("wl", 0.5, 0.5, 45))
+        builder.weapons.append(Weapon("fl", 0.5, -0.5, 315))
+        builder.weapons.append(Weapon("bl", -0.5, -0.5, 225))
+        builder.weapons.append(Weapon("br", -0.5, 0.5, 135))
+        builder.weapons.append(Weapon("fr", 0.5, 0.5, 45))
     
     def apply_stats(self, stats: RobotStats) -> None:
         stats.speed = 1
@@ -19,19 +19,20 @@ class MyRobot(RobotInterface):
         stats.energy_regen = 1
         stats.max_energy = 1
         stats.max_health = 1
+        stats.size = 1
 
     def do_ability(self, index: int, command_list: list[WeaponCommand]) -> None:
         if index == 1:
-            command_list.append(WeaponCommand("wf"))
+            command_list.append(WeaponCommand("fl"))
         elif index == 2:
-            command_list.append(WeaponCommand("wr"))
+            command_list.append(WeaponCommand("bl"))
         elif index == 3:
-            command_list.append(WeaponCommand("wb"))
+            command_list.append(WeaponCommand("br"))
         elif index == 4:
-            command_list.append(WeaponCommand("wl"))
+            command_list.append(WeaponCommand("fr"))
         elif index == 5:
-            command_list.append(WeaponCommand("wf"))
-            command_list.append(WeaponCommand("wr"))
+            command_list.append(WeaponCommand("fl"))
+            command_list.append(WeaponCommand("bl"))
         elif index == 6:
-            command_list.append(WeaponCommand("wb"))
-            command_list.append(WeaponCommand("wl"))
+            command_list.append(WeaponCommand("br"))
+            command_list.append(WeaponCommand("fr"))
