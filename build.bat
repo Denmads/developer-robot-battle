3cad6ac5f9097fafac7c716e40bb6ba309aa06f0
Denmads <43139465+Denmads@users.noreply.github.com>
2025-10-10 22:51:09 +0200

    fixed build script

--- build.bat
+++ build.bat
@@ -1,8 +1,10 @@
-Remove-Item -Path client\dist -Force -Recurse  
-Remove-Item -Path client\build -Force -Recurse  
+rmdir /s /q client\dist
+rmdir /s /q server\dist
 
 pyinstaller -F -n BattleBots --distpath client\dist --workpath client\build --icon client\app_icon.ico client\main.py
 robocopy client\robots client\dist\robots
 copy client\settings.json client\dist
+rmdir /s /q client\build
 
 pyinstaller -F -n BattleBots_Server --distpath server\dist --workpath server\build --icon client\app_icon.ico server\main.py
+rmdir /s /q server\build
