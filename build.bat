9c39add8e52ccf1fac1191f8077308025dee18c7
Denmads <43139465+Denmads@users.noreply.github.com>
2025-10-10 21:18:18 +0200

    server now buildable as exe

--- build.bat
+++ build.bat
@@ -4,3 +4,5 @@ Remove-Item -Path client\build -Force -Recurse
 pyinstaller -F -n BattleBots --distpath client\dist --workpath client\build --icon client\app_icon.ico client\main.py
 robocopy client\robots client\dist\robots
 copy client\settings.json client\dist
+
+pyinstaller -F -n BattleBots_Server --distpath server\dist --workpath server\build --icon client\app_icon.ico server\main.py
